64e04932ca1d03b1175e62f60ea166768a36d68b
boldlemur <136940936+boldlemur@users.noreply.github.com>
2024-04-29 09:51:52 +0800

    Changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-// App.js
+// Import the image and React library at the top of the file
 import React from 'react';
 import './App.css';
+import chaoskampf from '../public/chaoskampf.jpg'; // Adjust the path to where the image is located
 
 function App() {
+  // Use the imported image as a source
   return (
     <div className="App">
-      <header className="App-header" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/chaoskampf.jpg)` }}>
-        <h1>Hello World</h1> {/* Text to display over the background */}
+      <header className="App-header">
+        <img src={chaoskampf} alt="Chaoskampf" style={{ width: '100%', height: 'auto' }} />
+        <h1>Hello World</h1>
       </header>
     </div>
   );
 }
 
 export default App;
+
