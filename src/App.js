c0f60c2b59eea31dea1bd4fb11218d0dbb14706d
boldlemur <136940936+boldlemur@users.noreply.github.com>
2024-04-29 09:45:56 +0800

    Changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+// App.js
 import React from 'react';
 import './App.css';
+import chaoskampf from './chaoskampf.jpg'; // Make sure the path is correct
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>  {/* Text to display over the background */}
+      <header className="App-header" style={{ backgroundImage: `url(${chaoskampf})` }}>
+        <h1>Hello World</h1> {/* Text to display over the background */}
       </header>
     </div>
   );
 }
 
 export default App;
+
 
 
