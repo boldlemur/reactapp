47bece68035ea31d0e7794a6f7d9013d130e5cfc
boldlemur <136940936+boldlemur@users.noreply.github.com>
2024-04-29 09:49:23 +0800

    Changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 // App.js
 import React from 'react';
 import './App.css';
-import chaoskampf from './public/chaoskampf.jpg'; // Make sure the path is correct
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header" style={{ backgroundImage: `url(${chaoskampf})` }}>
+      <header className="App-header" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/chaoskampf.jpg)` }}>
         <h1>Hello World</h1> {/* Text to display over the background */}
       </header>
     </div>
@@ -14,6 +13,3 @@ function App() {
 }
 
 export default App;
-
-
-
